bc84962d9d87568117816ff2e56ec23c744fc917
8Gates <36276128+8Gates@users.noreply.github.com>
2021-09-02 21:28:34 -0500

    Update Get_A_PS5.py

--- Get_A_PS5.py
+++ Get_A_PS5.py
@@ -1,10 +1,13 @@
 import winsound
 import time
-import undetected_chromedriver as uc # pip install undetected-chromedriver
+from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc    # pip install undetected-chromedriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-username = 'BestBuy username or email'
-password = 'BestBuy password'
+
+username = ''
+password = ''
+sku_id = "'6426149'"    # sku-id for product to purchase
 count = 0
 continue_run = True
 options = uc.ChromeOptions()
@@ -12,42 +15,47 @@ options.headless = True
 headless_driver = uc.Chrome(options=options)
 url = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
 
+
 try:
     headless_driver.get(url)
 except:
     print("Error accessing URL.")
-    headless_driver.quit()  # close chrome browser
+    headless_driver.quit()
 
-def in_stock(headless_driver):
+def in_stock(headless_driver, sku):
     """Headless check if button displays sold out."""
-    element = WebDriverWait(headless_driver, 8).until(lambda d: d.find_element_by_xpath("//button[@data-sku-id='6426149']"))
+    element = WebDriverWait(headless_driver, 8).until(lambda d: d.find_element_by_xpath("//button[@data-sku-id="+sku+"]"))
     if element.text == "Sold Out":
         return False
     else:
         return True
 
-def add_to_cart(url):
+
+def add_to_cart(url, sku):
     # alert user stock may be available
-    winsound.Beep(440, 1000)
+    for i in range(3):
+        winsound.Beep(440, 200)
+    options = Options()                     
     options.headless = False
+    options.page_load_strategy = 'eager'    
     driver = uc.Chrome(options=options)
     driver.get(url)
     """Adds item to cart and checks out. Not headless."""
     try:
         # add item to the cart
-        driver.find_element_by_xpath("//button[@data-sku-id='6426149']").click()
-        time.sleep(4)
+        driver.find_element_by_xpath("//button[@data-sku-id="+sku+"]").click()
+        time.sleep(2)
         # go to the cart
         driver.find_element_by_xpath("//div[@class='go-to-cart-button']").click()
-        time.sleep(4)
+        time.sleep(2)
         # in cart checkout
         driver.find_element_by_xpath("//button[@data-track='Checkout - Top']").click()
-        time.sleep(4)
+        time.sleep(2)
         # sign in to profile
         driver.find_element_by_xpath("//input[@type='email']").send_keys(username)
         driver.find_element_by_xpath("//input[@type='password']").send_keys(password)
         driver.find_element_by_xpath("//button[@data-track='Sign In']").click()
-        time.sleep(4)
+        time.sleep(2)
         # place your order
         driver.find_element_by_xpath("//button[@data-track='Place your Order - Contact Card']").click()
 
@@ -63,12 +71,13 @@ def add_to_cart(url):
             user_input = input("An error occurred while checking out. Enter 'exit' to quit:")
         return
 
+
 while continue_run:
     # check for stock, add to cart and checkout
-    if in_stock(headless_driver):
-        add_to_cart(url)
+    if in_stock(headless_driver, sku_id):
+        add_to_cart(url, sku_id)
         continue_run = False
     else:
         count+=1
         print("Out of Stock. #"+ str(count))
-    time.sleep(60)
+    time.sleep(30)
